33b53d1031186c7f9cdc9134c76e5245d7a6a994
Jero <jero@MacBook-Pro-de-Jero.local>
2021-04-15 17:13:36 -0400

    ready navbar

---
+++ front/src/views/landing/inicio.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inicio = props => {
+    return(
+        <div className="initainer">
+            <h1>INICIO</h1>
+        </div>
+    )
+};
+
+export default Inicio;
