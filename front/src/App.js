31d200f205d0301830917f4f9db8296c2a649278
Jero <jero@MacBook-Pro-de-Jero.local>
2021-04-15 15:10:16 -0400

    first structure views and dependations intalled

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './views/home';
+import NotFound from './views/notfound';
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- front/src/index.js
+++ front/src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
