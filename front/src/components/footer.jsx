000d1657846192d9fe1c2c1b0eb8edbedfe3ae1d
Jero <jerosantamariai@gmail.com>
2021-04-28 20:49:36 -0400

    new advance in footer

--- front/src/components/footer.jsx
+++ front/src/components/footer.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import FlachaDerecha from '../img/smalleft.svg';
 
 const Footer = props => {
     return(
         <footer>
-            <h1>Footer</h1>
+            <div className="row no-gutters">
+                <div className="col-1">
+                    <img src={FlachaDerecha} alt="margen" className="margen" />
+                </div>
+                <div className="col-11 text-center my-auto">
+                    <h1>O CONTACTANOS AL</h1>
+                    <h3>+56912345678</h3>
+                    <h3>ABOGADOS@PROTEGEPYME.COM</h3>
+                </div>
+            </div>
         </footer>
     )
 };
