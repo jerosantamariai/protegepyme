5816ef8d63baecf1d2589497e915d6d56c9f3293
Jero <jerosantamariai@gmail.com>
2021-04-28 20:27:11 -0400

    new advance in xq

--- front/src/views/landing/xq.jsx
+++ front/src/views/landing/xq.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import FlechaDerecha from '../../img/bigleft.svg';
+import Cuadros from '../../img/cuadros.png';
 
 const XQ = props => {
     return(
         <div className="xqtainer" id="porquenosotros">
-            <h1>Por que trabajar con nosotros?</h1>
+            <div className="row no-gutters">
+                <div className="col-11 text-center">
+                    <h1>¿Porqué trabajar juntos?</h1>
+                    <h5>NUESTRO TRABAJO ES GARANTIA DE</h5>
+                    <h5>COMPROMISO, DEDICACIÓN Y EXPERIENCIA</h5>
+                    <img src={Cuadros} alt="cuadros"/>
+                </div>
+                <div className="col-1">
+                    <img src={FlechaDerecha} alt="marcodrecho"/>
+                </div>
+            </div>
         </div>
     )
 };
