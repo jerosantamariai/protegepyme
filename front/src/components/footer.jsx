00c108f847114cf7bc577a80790353567a2046d7
Jero <jerosantamariai@gmail.com>
2021-05-12 15:45:54 -0400

    adding a final provisional footer and fixing some troubles

--- front/src/components/footer.jsx
+++ front/src/components/footer.jsx
@@ -14,6 +14,14 @@ const Footer = props => {
                     <h4>ABOGADOS@PROTEGEPYME.COM</h4>
                 </div>
             </div>
+            <div className="row no-gutters">
+                {/* <div className="col-1">
+                    <img src={FlachaDerecha} alt="margen" className="margen" />
+                </div> */}
+                <div className="col-12 text-center my-auto realfooter">
+                    <p>Pagina diseñada y codificada por JJWebStudio</p>
+                </div>
+            </div>
         </footer>
     )
 };
