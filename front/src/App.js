04ea76e371ada7e9c839fa812f98335fbf455f5a
Jerónimo Santa María Illanes <jero@MacBook-Pro-de-Jeronimo.local>
2021-07-12 21:50:37 -0400

    adding a template for presentation

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Footer from './components/footer';
-import Navbar from './components/navbar';
+// import Footer from './components/footer';
+// import Navbar from './components/navbar';
 import injectContext from './store/appContext';
-import FakeUno from './views/fakeviews/fake1';
-import FakeDos from './views/fakeviews/fake2';
 import Home from './views/home';
 import NotFound from './views/notfound';
+import FakeUno from './views/fakeviews/fake1';
+import FakeDos from './views/fakeviews/fake2';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar />
+      {/* <Navbar /> */}
         <Switch>
             <Route exact path="/version2" component={FakeUno} />
             <Route exact path="/version3" component={FakeDos} />
             <Route exact path="/" component={Home} />
             <Route component={NotFound} />
         </Switch>
-        <Footer />
+        {/* <Footer /> */}
       </BrowserRouter>
     </div>
   );
